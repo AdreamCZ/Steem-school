7acab9283c0bfffdf2d540850fe1c89b238bcccd
Aram Denk <37027555+AdreamCZ@users.noreply.github.com>
2019-05-24 09:19:59 +0200

    Aded JS file

---
+++ js/Application.js
@@ -0,0 +1,10 @@
+class Application{
+	constructor(name,author){
+		this.name = name;
+		this.author = author;
+		}
+
+		getName(){ return this.name;}
+		
+
+}
---
+++ js/market.js
@@ -0,0 +1,14 @@
+var dataLink = "https://docs.google.com/spreadsheets/d/1P-6rgqU9mipblhcLXVVBQ8IYQm44gRhavxgeCECQbzw/edit?usp=sharing";
+
+	var app = new Application("Lolíček","Penka Lrochaskova");
+	var listDiv = document.getElementById("programs");
+	var appDiv = document.createElement("div");
+	var appName = document.createTextNode("tedt");
+	var appAuthor = document.createTextNode("Penka");
+	appDiv.appendChild(appName);
+	appDiv.appendChild(appAuthor);
+	
+	listDiv.appendChild(document.createTextNode("Test"));
+	
+	
+	
